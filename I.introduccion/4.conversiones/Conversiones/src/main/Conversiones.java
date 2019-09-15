3b95405018c0105a954e4b0355792da249a4d30c
Felipe mac <fconcha.almeida@gmail.com>
2019-09-14 23:16:11 -0300

    refactor: mejora de ejemplos

--- I.introduccion/4.conversiones/Conversiones/src/main/Conversiones.java
+++ I.introduccion/4.conversiones/Conversiones/src/main/Conversiones.java
@@ -10,7 +10,7 @@ public class Conversiones {
         int entero1;
         double flotanteDoble1;
 
-        flotanteDoble1 = (double) 7 / 9; //Exactitud: El resultado de la division entre dos datos tipo int es un numero entero, sin embargo hacemos casting a double para obtener un resultado exacto.
+        flotanteDoble1 = (double) 23 / 5; //Exactitud: El resultado de la division entre dos datos tipo int es un numero entero, sin embargo hacemos casting a double para obtener un resultado exacto.
         entero1 = (int) flotanteDoble1;  //Estimacion: Esto solo quita los decimales, sin redondear ni aproximar.
 
     // Casting automatico
