0f63f9bde44ad806eb36730d00a88ef1b94efd35
Felipe mac <fconcha.almeida@gmail.com>
2019-09-18 12:56:42 -0300

    refactor: reordenamiento de impresiones en consola

--- I.introduccion/2.operadores/Operadores/src/main/Operadores.java
+++ I.introduccion/2.operadores/Operadores/src/main/Operadores.java
@@ -3,44 +3,91 @@ package main;
 /**
  * @author Felipe Concha Almeida
  */
-
 public class Operadores {
     public static void main(String[] args) {
         int a = 9;
         int b;
 
-    //Operadores aritmeticos
-        System.out.print(a * 3); //Operador de multiplicacion: *                | Output: 27
-        System.out.print(a / 3); //Operador de divicion: /                      | Output: 3
-        System.out.print(a + 3); //Operador de suma: +                          | Output: 12
-        System.out.print(a - 3); //Operador de resta: -                         | Output: 6
-        System.out.print(a % 2); //Operador de modulo (residuo de division): %  | Output: 1
+        //Operadores aritmeticos
+        System.out.println("Operadores Aritmeticos: \n");
 
-    //Operadores de incremento(++) y decremento(--)
+        System.out.print("a = ");
+        System.out.println(a);
+
+        System.out.print("a * 3 = ");
+        System.out.println(a * 3); //Operador de multiplicacion: *                | Output: 27
+
+        System.out.print("a / 3 = ");
+        System.out.println(a / 3); //Operador de divicion: /                      | Output: 3
+
+        System.out.print("a + 3 = ");
+        System.out.println(a + 3); //Operador de suma: +                          | Output: 12
+
+        System.out.print("a - 3 = ");
+        System.out.println(a - 3); //Operador de resta: -                         | Output: 6
+
+        System.out.print("a % 2 = ");
+        System.out.println(a % 2); //Operador de modulo (residuo de division): %  | Output: 1
+
+        //Operadores de incremento(++) y decremento(--)
+        System.out.println("\nOperadores de Incremento y Decremento: \n");
+
         a = 3;
-        --a;
-        System.out.print(a); //Output: 2
+        System.out.print("a = ");
+        System.out.println(a);
+
+        System.out.print("--a = ");
+        System.out.println(--a); //Output: 2
 
         //Sufijos: realizan el incremento o decremento despues
-        System.out.print(a++); //Output: 2
-        System.out.print(a); //Output: 3
+        System.out.print("a++ = ");
+        System.out.println(a++); //Output: 2
+
+        System.out.print("a = ");
+        System.out.println(a); //Output: 3
 
         //Prefijos: realizan el incremento o decremento primero
-        System.out.print(++a); //Ouput 4
+        System.out.print("++a = ");
+        System.out.println(++a); //Ouput 4
 
-    //Operadores Relacionales
+        //Operadores Relacionales
+        System.out.println("\nOperadores Relacionales: \n");
+
         a = 8;
-        b = 2;   
-        
-        System.out.println("a es mayor a b ? " + (a > b));
-        System.out.println("a es menor a b ? " + (a < b));
-        System.out.println("a es mayor o igual que b ? " + (a >= b));
-        System.out.println("a es menor o igual que b ? " + (a <= b));
-        System.out.println("a es igual que b ? " + (a == b));
-        System.out.println("a es diferente que b ? " + (a != b));
+        b = 2;
 
-    //Operadores Logicos
-        System.out.println("Se cumple la condicion 1  Y  condicion 2: " + ((a == 8) && (b == 2))); //Operador AND: &&  
-        System.out.println("Se cumple la condicion 1  O  la condicion 2: " + ((a == 2) || (b == 2))); //Operador OR: ||        
+        System.out.print("a = ");
+        System.out.println(a);
+        System.out.print("b = ");
+        System.out.println(b);
+
+        System.out.print("a es mayor a b ? :");
+        System.out.println(a > b);
+
+        System.out.print("a es menor a b ? :");
+        System.out.println(a < b);
+
+        System.out.print("a es mayor o igual que b ? :");
+        System.out.println(a >= b);
+
+        System.out.print("a es menor o igual que b ? :");
+        System.out.println(a <= b);
+
+        System.out.print("a es igual que b ? :");
+        System.out.println(a == b);
+
+        System.out.print("a es diferente que b ? :");
+        System.out.println(a != b);
+
+        //Operadores Logicos
+        System.out.println("\nOperadores Logicos\n");
+
+        System.out.print("a = ");
+        System.out.println(a);
+        System.out.print("b = ");
+        System.out.println(b);
+
+        System.out.println("Se cumple la condicion1 (a = 8)?  Y  condicion2 (b = 2)? : " + ((a == 8) && (b == 2))); //Operador AND: &&  
+        System.out.println("Se cumple la condicion1 (a = 2)? O  la condicion2 (b = 2)? : " + ((a == 2) || (b == 2))); //Operador OR: ||        
     }
 }
