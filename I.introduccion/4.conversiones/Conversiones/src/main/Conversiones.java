fc996b51362c05469b1d7dfb21a8c741b1816789
Felipe mac <fconcha.almeida@gmail.com>
2019-09-18 13:18:22 -0300

    refactor: se mejoraron los ejemplo y se agregaron mas impresiones en consola

--- I.introduccion/4.conversiones/Conversiones/src/main/Conversiones.java
+++ I.introduccion/4.conversiones/Conversiones/src/main/Conversiones.java
@@ -10,21 +10,31 @@ public class Conversiones {
         int entero1;
         double flotanteDoble1;
 
-        flotanteDoble1 = (double) 23 / 5; //Exactitud: El resultado de la division entre dos datos tipo int es un numero entero, sin embargo hacemos casting a double para obtener un resultado exacto.
+        flotanteDoble1 = (double) 23 / 9; //Exactitud: El resultado de la division entre dos datos tipo int es un numero entero, sin embargo hacemos casting a double para obtener un resultado exacto.
+        System.out.println("Exactitud: \n");        
+        System.out.println("flotanteDoble1 = " + flotanteDoble1);
+        
         entero1 = (int) flotanteDoble1;  //Estimacion: Esto solo quita los decimales, sin redondear ni aproximar.
-
+        System.out.println("\nEstimacion: \n");        
+        System.out.println("entero1 = " + entero1);
+        
     // Casting automatico
         char caracter1;
         int caracterEntero1;
+     
+        caracter1 = '1';      
+        caracterEntero1 = caracter1;  //De char a int, se efectua casting automatico.
+        
         double flotanteDoble2;
         int entero2;
-
+        
         entero2 = 235;
-        caracter = '1';      
+        flotanteDoble2 = entero2;  //De int a double, se efectua casting automatico.
 
-        caracterEntero = caracter;  //De char a int, se efectua casting automatico.
-        flotanteDoble2 =  entero2;  //De int a double, se efectua casting automatico.
- 
+        System.out.println("\nCasting automatico: \n");
+        System.out.println("entero2 = " + entero2);
+        System.out.println("flotanteDoble2 = " + flotanteDoble2);  
+        
     // Parsing 
         int entero3;
         double flotanteDoble3;
@@ -33,6 +43,9 @@ public class Conversiones {
         cadena = "255";
         
         entero3 = Integer.parseInt(cadena);  //Hacemos un parsing de String a int
-        flotanteDoble3 = Double.parseFloat("3.56");  //Hacemos un parging de String literal a float
+        flotanteDoble3 = Double.parseDouble("3.536");  //Hacemos un parging de String literal a float
+        
+        System.out.println("\nParsing: \n");
+        System.out.println("flotanteDoble3 = " + flotanteDoble3);
     }
 }
