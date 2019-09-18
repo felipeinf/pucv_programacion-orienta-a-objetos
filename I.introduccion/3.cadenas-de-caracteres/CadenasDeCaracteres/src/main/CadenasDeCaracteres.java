88f9d1a2bfb6bc0b446973231f83c26194902ec6
Felipe mac <fconcha.almeida@gmail.com>
2019-09-18 12:59:06 -0300

    refactor: se mejoraron los ejemplos impresos por consola

--- I.introduccion/3.cadenas-de-caracteres/CadenasDeCaracteres/src/main/CadenasDeCaracteres.java
+++ I.introduccion/3.cadenas-de-caracteres/CadenasDeCaracteres/src/main/CadenasDeCaracteres.java
@@ -7,19 +7,26 @@ package main;
 public class CadenasDeCaracteres {
 
     public static void main(String[] args) {
-        String cadena1 = "Soy una cadena inicializada"; //Una cadena se puede inicializar.
+        String cadena1 = "Soy una cadena inicializada "; //Una cadena se puede inicializar.
         String cadena2;
         String cadena3;
 
         cadena2 = "Soy una cadena asignada"; //Una cadena se puede declarar y asignar posteriormente.
 
-        System.out.println(cadena1 + " y " + cadena2); //Al imprimir, se pueden concatenar dos o mas cadenas utilizando el operador + .
-        System.out.println("Cadena 1 es igual a cadena 2: " + (cadena1.equals(cadena2))); //cadena1.equals(cadena2) retorna true en caso de ser iguales y false en caso contrario.
+        System.out.println(cadena1 + cadena2); //Al imprimir, se pueden concatenar dos o mas cadenas utilizando el operador + .
+       
+        System.out.println("\nCadena1 = " + cadena1);
+        System.out.println("Cadena2 = " + cadena2);
+        System.out.println("Cadena1 es igual a cadena2: " + (cadena1.equals(cadena2))); //cadena1.equals(cadena2) retorna true en caso de ser iguales y false en caso contrario.
         
-        cadena1 = cadena1.toLowerCase(); //Una cadena se puede convertir en Mayuscula.
-        cadena2 = cadena2.toUpperCase(); //Una cadena se puede convertir en Minuscula.
+        cadena1 = cadena1.toLowerCase(); //Una cadena se puede convertir en Minuscula.
+        System.out.println("\nCadena1 en minuscula: "+ cadena1);
+        
+        cadena2 = cadena2.toUpperCase(); //Una cadena se puede convertir en Mayuscula.
+        System.out.println("Cadena2 en mayuscula: " + cadena2);
 
         cadena3 = "Hola";
-        System.out.println("El largo de cadena 3 es :" + cadena3.length()); //Output: 4
+        System.out.println("\nCadena3 = " + cadena3);
+        System.out.println("El largo de cadena 3 es: " + cadena3.length()); //Output: 4
     }
 }
